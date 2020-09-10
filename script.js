e6f8e8b90b4b339fe8b8573516960c5903b6d979
gcancho <giovannicb19@gmail.com>
2020-09-10 12:53:33 -0500

    loading con keyframes y js

---
+++ script.js
@@ -0,0 +1,7 @@
+const contenedor_loader = document.querySelector('.contenedor_loader');
+
+window.addEventListener('load',function(){
+    contenedor_loader.style= "opacity: 0;";
+});
+
+// setTimeout(function(){ alert("Hello"); }, 3000);
